165a0d62e3c06c1271378a48a1fbcecc5b2e784a
gundarsv <267009@viauc.dk>
2020-05-04 23:19:03 +0200

    Added possibility for manufacturers request and some cleanup for flights

--- Controller/PlanesController.py
+++ Controller/PlanesController.py
@@ -15,6 +15,17 @@ class PlanesController(planes_pb2_grpc.PlanesServicer):
     def GetPlanes(self, request, context):
         all_planes = self.repository.get_all_planes()
         grpc_planes = []
+
         for data in all_planes:
             grpc_planes.append(planes_pb2.Plane(tailnum=data.tailnum, year=data.year, type=data.type, manufacturer=data.manufacturer, model=data.model,engines=data.engines, seats=data.seats, speed=data.speed, engine=data.engine))
+
         return planes_pb2.PlaneResponse(planes=grpc_planes)
+
+    def GetManufacturersWithMoreThan200Planes(self, request, context):
+        manufacturers_with_more_than_200_planes = self.repository.get_manufacturers_with_more_than_200_planes()
+        grpc_manufacturers_with_more_than_200_planes = []
+
+        for data in manufacturers_with_more_than_200_planes:
+            grpc_manufacturers_with_more_than_200_planes.append(planes_pb2.Manufacturer(planes=data.planesTotal, manufacturer=data.manufacturer))
+
+        return planes_pb2.ManufacturersResponse(manufacturers=grpc_manufacturers_with_more_than_200_planes)
